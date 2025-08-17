94256b7895c54429fb92d5c0ebf659bee069a01d
Joseph Corritone <jcorritone@gmail.com>
2025-08-16 18:58:47 -0700

    Import CSV and Prepare Data

--- src/import-csv.R
+++ src/import-csv.R
@@ -1,10 +1,11 @@
-# Import CSV File (Replace "replace" with file name), Adjust Start Percent & Rostered Percent from a percent to a proportion
+# Import CSV File (Replace "replace" with file name), Select relevant columns & adjust start_percent from a 0-100 scale to a 0-1 scale.
 
 library(tidyverse)
 
-data <- read_csv("replace.csv")
-data <- data %>%
-  mutate(
-    start_percent = as.numeric(start_percent) / 100,
-    pct_rostered = as.numeric(pct_rostered) / 100
-  )
+data = read_csv("replace.csv")
+
+cleaned_data = data %>%
+  select(player_name, team, position, points, start_percent)
+
+cleaned_data = cleaned_data %>%
+  mutate(start_percent = as.numeric(start_percent) / 100)
